af89961e28bb51a9c09de97b271afc4cee1698a6
Joseph Ji <jji@lcs.on.ca>
2025-01-20 09:21:54 -0500

    Initial Commit

---
+++ DIce Roller App/DIce_Roller_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIce_Roller_AppApp.swift
+//  DIce Roller App
+//
+//  Created by Xinchen Ji on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct DIce_Roller_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
