0b20b260207047cbd682f1128c463bc802ae2139
Joseph Ji <jji@lcs.on.ca>
2025-01-21 12:24:56 -0500

    Finished the function of rolling the dice.

--- DIce Roller App/RollingView.swift
+++ DIce Roller App/RollingView.swift
@@ -17,13 +17,19 @@ struct RollingView: View {
     @State var condition5 = Int.random(in: 5...30)
     @State var condition6 = Int.random(in: 6...36)
     @State var feedback = ""
-    @State var rollHistory: [Int] = []
+    @State var dicesRolled: [Int] = []
     
     var body: some View {
         NavigationStack {
             VStack {
                 Text("Roll the dice, up to 6.")
+                
                 Spacer()
+                
+                Text(feedback)
+                
+                Spacer()
+                
                 HStack {
                     Stepper(value: $base, in: 1...6, label: {
                         Text("Dice Number")
@@ -31,11 +37,63 @@ struct RollingView: View {
                     Spacer(minLength: 20)
                     Text("\(base)")
                 }
+                
+                Spacer()
+                
+                Button {
+                    roll()
+                } label: {
+                    Text("Roll")
+                }
+                
+                Spacer()
+                
+                Button {
+                    reset()
+                } label: {
+                    Text("Reset")
+                }
+                
+                Spacer()
+                
+                Text("Dices Rolled")
+                ScrollView{
+                    VStack(spacing: 5){
+                        ForEach(dicesRolled, id: \.self) { currentDice in
+                            Text("\(currentDice)")
+                            Divider()
+                        }
+                    }
+                }
+                
             }
             .padding()
             .navigationTitle("Dice Roller")
         }
     }
+    
+    func roll() {
+        if base == 1 {
+            feedback = "Total number is: \(condition1)"
+            dicesRolled.append(condition1)
+        } else if base == 2 {
+            feedback = "Total number is: \(condition2)"
+            dicesRolled.append(condition2)
+        } else if base == 3 {
+            feedback = "Total number is: \(condition3)"
+            dicesRolled.append(condition3)
+        } else if base == 4 {
+            feedback = "Total number is: \(condition4)"
+            dicesRolled.append(condition4)
+        } else if base == 5 {
+            feedback = "Total number is: \(condition5)"
+            dicesRolled.append(condition5)
+        } else if base == 6 {
+            feedback = "Total number is: \(condition6)"
+            dicesRolled.append(condition6)
+        }
+    }
+
 }
 
 #Preview {
